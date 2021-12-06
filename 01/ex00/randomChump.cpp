6399dfa02ad1fc55f47579a70efe6fa7cdcbf397
momeyer <meyer.monique93@gmail.com>
2021-12-06 14:51:01 +0100

    last

--- 01/ex00/randomChump.cpp
+++ 01/ex00/randomChump.cpp
@@ -1,8 +1,8 @@
 #include "Zombie.hpp"
 
-void randomChump( std::string name )
+void
+randomChump(std::string name)
 {
-    Zombie* zombie = newZombie(name);
-    zombie->announce();
-    delete zombie;
+  Zombie zombie(name);
+  zombie.announce();
 }
