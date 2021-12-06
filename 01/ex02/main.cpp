9500c3703070404e70d616ef718409e5143c8ac9
momeyer <meyer.monique93@gmail.com>
2021-12-06 14:53:36 +0100

    last

--- 01/ex02/main.cpp
+++ 01/ex02/main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 
-int main()
+int
+main()
 {
-    std::string string = "HI THIS IS BRAIN";
-    std::string *stringPTR = &string;
-    std::string& stringREF = string;
+  std::string string = "HI THIS IS BRAIN";
+  std::string* stringPTR = &string;
+  std::string& stringREF = string;
 
-    std::cout << "This is the address of string: " << &string << std::endl;
+  std::cout << "This is the address of string: " << &string << std::endl;
 
-    std::cout << "This is the address of stringPTR: " << &stringPTR << std::endl;
-    std::cout << "This is the address of stringREF: " << &stringREF << std::endl;
+  std::cout << "This is the address of string using stringPTR: " << stringPTR
+	    << std::endl;
+  std::cout << "This is the address of string using  stringREF: " << &stringREF
+	    << std::endl;
 
-    std::cout << "This is the string using stringPTR: " << *stringPTR << std::endl;
-    std::cout << "This is the string using stringREF: " << stringREF << std::endl;
+  std::cout << "This is the string using stringPTR: " << *stringPTR
+	    << std::endl;
+  std::cout << "This is the string using stringREF: " << stringREF << std::endl;
 }
