f7f93d048ca0a4b928c1b1980313345389337104
momeyer <meyer.monique93@gmail.com>
2022-01-13 19:18:29 +0100

    added tests to main

--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -1,39 +1,52 @@
 #include "mutantstack.hpp"
 #include <assert.h>
 #include <iostream>
-#include <vector>
+#include <list>
+
 int
 main()
 {
-  MutantStack<int> myStack;
+  typedef MutantStack<int> MyStack;
+  typedef std::list<int> Container;
 
-  std::vector<int> vec;
-  vec.push_back(5);
-  vec.push_back(17);
-  vec.push_back(3);
-  vec.push_back(5);
-  vec.push_back(737);
-  vec.push_back(0);
+  Container container;
+  container.push_back(5);
+  container.push_back(17);
+  container.push_back(3);
+  container.push_back(5);
+  container.push_back(737);
+  container.push_back(0);
 
-  std::vector<int>::iterator v_it;
-
-  for (v_it = vec.begin(); v_it != vec.end(); ++v_it) {
-    MutantStack<int>::iterator s_ite = myStack.end();
-
-    myStack.push(*v_it);
-    std::cout << *s_ite << " == " << *v_it << '\n';
-    assert(*(myStack.end() - 1) == *v_it);
+  MyStack myStack;
+  for (Container::iterator it = container.begin(); it != container.end();
+       ++it) {
+    myStack.push(*it);
   }
 
-  MutantStack<int>::iterator it = myStack.begin();
-  MutantStack<int>::iterator ite = myStack.end();
-  ++it;
-  --it;
-  v_it = vec.begin();
-  while (it != ite) {
-    assert(*it == *v_it);
-    std::cout << *it << " == " << *v_it << '\n';
-    ++it;
-    ++v_it;
+  MyStack::iterator it_stack = myStack.begin();
+
+  std::cout << "\nForward Iterator:\n\n";
+  for (Container::iterator it_container = container.begin();
+       it_container != container.end() && it_stack != myStack.end();
+       ++it_container, ++it_stack) {
+    assert(*it_stack == *it_container);
+    std::cout << "stack " << *it_stack << " == "
+	      << "container " << *it_container << "\n";
   }
+  std::cout << "\nReverse Iterator:\n\n";
+  MyStack::reverse_iterator rit_stack = myStack.rbegin();
+  for (Container::reverse_iterator rit_container = container.rbegin();
+       rit_container != container.rend() && rit_stack != myStack.rend();
+       ++rit_container, ++rit_stack) {
+    assert(*rit_stack == *rit_container);
+    std::cout << "stack " << *rit_stack << " == "
+	      << "container " << *rit_container << "\n";
+  }
+
+  std::cout << "\nStack Operations:\n\n";
+  std::cout << "Stack size: " << myStack.size() << '\n';
+  std::cout << "Stack top: " << myStack.top() << '\n';
+  std::cout << "Poping element\n";
+  myStack.pop();
+  std::cout << "Stack top: " << myStack.top() << '\n';
 }
--- 08/ex02/mutantstack.hpp
+++ 08/ex02/mutantstack.hpp
@@ -10,13 +10,17 @@ public:
   typedef typename BaseClass::container_type Container;
   typedef typename Container::iterator iterator;
   typedef typename Container::const_iterator const_iterator;
+  typedef typename Container::reverse_iterator reverse_iterator;
+  typedef typename Container::const_reverse_iterator const_reverse_iterator;
 
   iterator begin() { return this->c.begin(); };
   const_iterator begin() const { return this->c.begin(); };
-  iterator rbegin() { return this->c.rbegin(); };
+  reverse_iterator rbegin() { return this->c.rbegin(); };
+  const_reverse_iterator rbegin() const { return this->c.rbegin(); };
   iterator end() { return this->c.end(); };
   const_iterator end() const { return this->c.end(); };
-  iterator rend() { return this->c.rend(); };
+  reverse_iterator rend() { return this->c.rend(); };
+  const_reverse_iterator rend() const { return this->c.rend(); };
 };
 
 #endif // MUTANTSTACK_H
